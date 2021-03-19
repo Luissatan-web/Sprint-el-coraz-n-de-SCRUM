7eda1e3f31971089d9795da774b9cf43b6c0a9a2
luis flores <luis.fm13@outlook.com>
2021-03-19 14:43:17 -0600

    subiendo archivos

--- src/App.js
+++ src/App.js
@@ -3,22 +3,81 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <header>
+      <nav>
+        <div className="logo">
+          <img src="img/faw.png">
+          </img>
+        </div>
+        <div className="menu">
+          <a href="#">
+              Inicio
+          </a>
+          <a href="#">
+            Camiones
+          </a>
+          <a href="#">
+            Refacciones
+          </a>
+          <a href="#">
+            Ubicaciones
+          </a>
+          <a href="#">
+            Contacto
+          </a>
+        </div>
+      </nav>
+      <div className="cabecera">
+      <img  src="img/volvo.jpg">
+      </img>
+      </div>
+    </header>
+    <section>
+      <h2>
+            CONOCENOS SOMOS FAW MÉXICO
+      </h2>
+      <div className="informacion">
+          <div className="informacion_imagen">
+              <img src="img/servicio.jpg">
+              </img>
+          </div>
+          <div className="informacion_texto">
+              <p>
+              En FAW México contamos con el personal mejor capacitado para la reparación de tu vehículo, te invitamos a que te acerques a cualquiera de nuestras sucursales donde un asesor de servicios
+                te brindara la información y atención necesaria para que notes que FAW México es la diferencia.
+                De igual manera contamos con una gran variedad de unidades a un costo accesible que se ajustara a tus necesidades y a tu bolsillo.
+              </p>
+          </div>
+      </div>
+    </section>
+    <footer>
+      <div className="telefonos">
+        <h3>Télefono</h3>
+        <p>555-555-5555</p>
+        <p>555-555-5555</p>
+        <p>555-555-5555</p>
+      </div>
+      <div className="correos">
+        <h3>Correos</h3>
+        <p>correo@correo.es</p>
+        <p>correo@correo.es</p>
+        <p>correo@correo.es</p>
+      </div>
+      <div className="sucursales">
+        <h3>Sucursales</h3>
+        <p>Guadalajara</p>
+        <p>Tlaquepaque</p>
+        <p>Zapopan</p>
+      </div>
+      <div className="redes">
+          <img src="img/face.png"></img>
+          <img src="img/insta.png"></img>
+          <img src="img/twit.png"></img>
+          <img src="img/you.png"></img>
+      </div>
+    </footer>
+   </>
   );
 }
 
